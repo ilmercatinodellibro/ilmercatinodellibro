c720632842e8d9ad491659959434e5bca4d0778e
andrea.cerlini <andreacerlini2003@gmail.com>
2023-11-23 11:16:46 +0100

    feat(catalog): add main catalog UI

--- packages/client/src/i18n/en-US/book.ts
+++ packages/client/src/i18n/en-US/book.ts
@@ -2,4 +2,26 @@ export default {
   loading: "Loading books",
   loadBooksButton: "Load books into database",
   noResult: "No books found",
+  columns: {
+    isbn: "ISBN Code",
+    author: "Author",
+    publisher: "Publisher",
+    subject: "Subject",
+    title: "Title",
+    price: "Cover Price",
+    status: "Status",
+    utility: "Utility",
+  },
+  search: "Search",
+  addBook: "Add a book",
+  filter: "Filter",
+  utility: {
+    high: "High",
+    medium: "Medium",
+    low: "Low",
+  },
+  availability: {
+    available: "Available",
+    notAvailable: "Not Available",
+  },
 };
