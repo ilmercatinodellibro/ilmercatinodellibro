f8c431c41bb02fb1d8159285264001bcd53d030b
andrea.cerlini <andreacerlini2003@gmail.com>
2024-01-09 17:24:08 +0100

    fix(book): add k-form-dialog-card as wrapper for dialogs + other fixes based on PR

--- packages/client/src/components/filter-by-school-dialog.vue
+++ packages/client/src/components/filter-by-school-dialog.vue
@@ -1,54 +1,46 @@
 <template>
   <q-dialog ref="dialogRef" @hide="onDialogHide">
-    <q-card class="dialog-card">
-      <q-card-section class="q-px-lg q-py-md">
-        <div class="tex-primary text-h6">
-          {{ $t("book.filters.school") }}
-        </div>
-      </q-card-section>
-      <q-separator />
-      <q-card-section class="q-col-gutter-lg q-pa-lg">
+    <k-dialog-form-card
+      :title="$t('book.filters.school')"
+      :save-label="$t('book.filter')"
+      size="sm"
+      @save="onDialogOK(newFilters)"
+      @cancel="onDialogCancel"
+    >
+      <q-card-section class="q-gutter-md">
         <q-select
-          v-for="(filter, index) in filters"
-          :key="index"
-          v-model="newFilters[index]"
-          :label="
-            $t(
-              `book.filters.schoolFilter.fields.${
-                index === 0 ? 'address' : 'school'
-              }`,
-            )
-          "
-          :options="filter"
+          v-model="newFilters.addresses"
+          :label="$t('book.filters.schoolFilter.fields.address')"
+          :options="filters.addresses"
+          fill-input
+          multiple
+          outlined
+        />
+
+        <q-select
+          v-model="newFilters.schoolCodes"
+          :label="$t('book.filters.schoolFilter.fields.school')"
+          :options="filters.schoolCodes"
           fill-input
           multiple
           outlined
         />
       </q-card-section>
-      <q-separator />
-      <q-card-actions align="right">
-        <q-btn :label="$t('common.cancel')" flat @click="onDialogCancel()" />
-        <q-btn
-          :label="$t('book.filter')"
-          flat
-          @click="onDialogOK(newFilters)"
-        />
-      </q-card-actions>
-    </q-card>
+    </k-dialog-form-card>
   </q-dialog>
 </template>
 
 <script setup lang="ts">
-import { cloneDeep } from "lodash-es";
 import { useDialogPluginComponent } from "quasar";
 import { ref } from "vue";
+import KDialogFormCard from "./k-dialog-form-card.vue";
 
 const props = defineProps<{
-  filters: string[][];
-  selectedFilters: string[][] | undefined;
+  filters: { schoolCodes: string[]; addresses: string[] };
+  selectedFilters?: { schoolCodes: string[]; addresses: string[] };
 }>();
 
-const newFilters = ref(cloneDeep(props.selectedFilters) ?? ([] as string[][]));
+const newFilters = ref(props.selectedFilters ?? ({} as typeof props.filters));
 
 const { dialogRef, onDialogCancel, onDialogOK, onDialogHide } =
   useDialogPluginComponent();
--- packages/client/src/components/utility-chip.vue
+++ packages/client/src/components/utility-chip.vue
@@ -2,11 +2,11 @@
   <q-chip
     square
     :ripple="false"
-    :color="colorFromValue(value).color"
-    :dark="colorFromValue(value).color !== 'yellow'"
-    class="utility-chip"
+    :color="colorFromValue(value ?? 0).color"
+    :dark="colorFromValue(value ?? 0).color !== 'yellow'"
+    class="line-height-16 text-size-14 utility-chip"
   >
-    {{ $t(`book.utility.${colorFromValue(value).label}`) }}
+    {{ $t(`book.utility.${colorFromValue(value ?? 0).label}`) }}
   </q-chip>
 </template>
 
@@ -15,14 +15,21 @@ const UTILITY_LOW_THRESHOLD = 0.33;
 const UTILITY_HIGH_THRESHOLD = 0.66;
 
 defineProps<{
-  value: "red" | "yellow" | "green";
+  value?: number;
 }>();
 
-function colorFromValue(value: string) {
-  return parseFloat(value) < UTILITY_LOW_THRESHOLD
+function colorFromValue(value: number) {
+  return value < UTILITY_LOW_THRESHOLD
     ? { color: "red", label: "low" }
-    : parseFloat(value) < UTILITY_HIGH_THRESHOLD
+    : value < UTILITY_HIGH_THRESHOLD
     ? { color: "yellow", label: "medium" }
     : { color: "green", label: "high" };
 }
 </script>
+
+<style scoped lang="scss">
+.utility-chip {
+  text-transform: uppercase;
+  user-select: none;
+}
+</style>
