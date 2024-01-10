d619a0a04c5fefdf4871693f412451cb7f8bf108
andrea.cerlini <andreacerlini2003@gmail.com>
2024-01-10 14:52:52 +0100

    fix(edit user): use k-form-dialog-card for edit user dialog

--- packages/client/src/components/manage-users/edit-user-details-dialog.vue
+++ packages/client/src/components/manage-users/edit-user-details-dialog.vue
@@ -1,100 +1,90 @@
 <template>
   <q-dialog ref="dialogRef" persistent @hide="onDialogHide">
-    <q-card class="form-card">
-      <q-card-section>
-        <div class="text-h6 text-primary">
-          {{ $t("manageUsers.editUser.title") }}
-        </div>
+    <k-dialog-form-card
+      :title="$t('manageUsers.editUser.title')"
+      :submit-label="$t('common.save')"
+      size="sm"
+      autocorrect="off"
+      autocapitalize="off"
+      autocomplete="off"
+      spellcheck="false"
+      @submit="
+        onDialogOK({
+          user: newUserData.user,
+          password: newUserData.newPassword,
+        })
+      "
+      @cancel="onDialogCancel"
+    >
+      <q-card-section class="q-gutter-md">
+        <q-input
+          v-model="newUserData.user.firstname"
+          :label="$t('manageUsers.fields.firstName')"
+          :rules="[requiredRule]"
+          clearable
+          outlined
+        />
+        <q-input
+          v-model="newUserData.user.lastname"
+          :label="$t('manageUsers.fields.lastName')"
+          :rules="[requiredRule]"
+          clearable
+          outlined
+        />
+        <q-input
+          v-model="newUserData.user.email"
+          :label="$t('manageUsers.fields.email')"
+          :rules="[requiredRule, emailRule]"
+          clearable
+          outlined
+        />
+        <q-input
+          v-model="newUserData.user.phoneNumber"
+          :label="$t('manageUsers.fields.phoneNumber')"
+          bottom-slots
+          mask="phone"
+          outlined
+        />
+        <q-input
+          v-model="newUserData.newPassword"
+          :label="$t('auth.password')"
+          :rules="[newUserData.newPassword ? validatePasswordRule : () => true]"
+          :type="hidePassword ? 'password' : 'text'"
+          outlined
+          autocomplete="off"
+        >
+          <template #append>
+            <q-icon
+              :name="hidePassword ? 'mdi-eye-off' : 'mdi-eye'"
+              class="cursor-pointer"
+              autocomplete="off"
+              @click="hidePassword = !hidePassword"
+            />
+          </template>
+        </q-input>
+        <q-input
+          v-model="newUserData.confirmPassword"
+          :label="$t('auth.confirmPassword')"
+          :rules="[
+            makeValueMatchRule(
+              newUserData.newPassword,
+              $t('auth.passwordDoNotMatch'),
+            ),
+          ]"
+          :type="hideConfirm ? 'password' : 'text'"
+          outlined
+          autocomplete="off"
+        >
+          <template #append>
+            <q-icon
+              :name="hideConfirm ? 'mdi-eye-off' : 'mdi-eye'"
+              class="cursor-pointer"
+              @click="hideConfirm = !hideConfirm"
+            />
+          </template>
+        </q-input>
       </q-card-section>
-      <q-separator />
-      <q-form
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="off"
-        spellcheck="false"
-        @submit="
-          onDialogOK({
-            user: newUserData.user,
-            password: newUserData.newPassword,
-          })
-        "
-      >
-        <q-card-section class="q-pb-none">
-          <q-input
-            v-model="newUserData.user.firstname"
-            :label="$t('manageUsers.fields.firstName')"
-            :rules="[requiredRule]"
-            clearable
-            outlined
-          />
-          <q-input
-            v-model="newUserData.user.lastname"
-            :label="$t('manageUsers.fields.lastName')"
-            :rules="[requiredRule]"
-            clearable
-            outlined
-          />
-          <q-input
-            v-model="newUserData.user.email"
-            :label="$t('manageUsers.fields.email')"
-            :rules="[requiredRule, emailRule]"
-            clearable
-            outlined
-          />
-          <!-- TODO: add phone number -->
-          <q-input
-            v-model="newUserData.user.phoneNumber"
-            :label="$t('manageUsers.fields.phoneNumber')"
-            bottom-slots
-            mask="phone"
-            outlined
-          />
-          <q-input
-            v-model="newUserData.newPassword"
-            :label="$t('auth.password')"
-            :rules="[
-              newUserData.newPassword ? validatePasswordRule : () => true,
-            ]"
-            :type="hidePassword ? 'password' : 'text'"
-            outlined
-          >
-            <template #append>
-              <q-icon
-                :name="hidePassword ? 'mdi-eye-off' : 'mdi-eye'"
-                class="cursor-pointer"
-                autocomplete="off"
-                @click="hidePassword = !hidePassword"
-              />
-            </template>
-          </q-input>
-          <q-input
-            v-model="newUserData.confirmPassword"
-            :label="$t('auth.confirmPassword')"
-            :rules="[
-              makeValueMatchRule(
-                newUserData.newPassword,
-                $t('auth.passwordDoNotMatch'),
-              ),
-            ]"
-            :type="hideConfirm ? 'password' : 'text'"
-            outlined
-          >
-            <template #append>
-              <q-icon
-                :name="hideConfirm ? 'mdi-eye-off' : 'mdi-eye'"
-                class="cursor-pointer"
-                @click="hideConfirm = !hideConfirm"
-              />
-            </template>
-          </q-input>
-        </q-card-section>
-        <q-separator />
-        <q-card-actions align="right">
-          <q-btn :label="$t('common.cancel')" flat @click="onDialogCancel" />
-          <q-btn :label="$t('common.confirm')" flat type="submit" />
-        </q-card-actions>
-      </q-form>
-    </q-card>
+    </k-dialog-form-card>
   </q-dialog>
 </template>
 
@@ -109,6 +99,7 @@ import {
   validatePasswordRule,
 } from "src/helpers/rules";
 import { UserFragment } from "src/services/user.graphql";
+import KDialogFormCard from "../k-dialog-form-card.vue";
 
 const props = defineProps<{
   userData: UserFragment;
@@ -125,7 +116,7 @@ const hideConfirm = ref(true);
 const { dialogRef, onDialogCancel, onDialogHide, onDialogOK } =
   useDialogPluginComponent();
 
-defineEmits(useDialogPluginComponent.emitsObject);
+defineEmits({ ...useDialogPluginComponent.emitsObject });
 </script>
 
 <style scoped lang="scss">
