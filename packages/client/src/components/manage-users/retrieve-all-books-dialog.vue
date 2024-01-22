ff6eb56c835c48926c0b1f38f6c3589fa6a277e7
andrea.cerlini <andreacerlini2003@gmail.com>
2024-01-22 15:49:05 +0100

    fix(retrieve-all-books-dialog): use k-dialog-card

--- packages/client/src/components/manage-users/retrieve-all-books-dialog.vue
+++ packages/client/src/components/manage-users/retrieve-all-books-dialog.vue
@@ -1,15 +1,11 @@
 <template>
   <q-dialog ref="dialogRef" @hide="onDialogHide">
-    <q-card>
-      <q-card-section class="text-h6 text-primary">
-        {{ $t("book.retrieveBooksDialog.title") }}
-      </q-card-section>
-
+    <k-dialog-card :title="$t('book.retrieveBooksDialog.title')">
       <q-card-section>
         {{ $t("book.retrieveBooksDialog.message") }}
       </q-card-section>
 
-      <q-card-actions class="no-wrap">
+      <template #card-actions>
         <q-btn
           outlined
           no-wrap
@@ -19,22 +15,23 @@
         <q-space />
         <q-btn flat :label="$t('common.cancel')" @click="onDialogCancel" />
         <q-btn no-wrap color="accent" @click="onDialogOK(true)">
-          <q-icon name="mdi-information-outline" class="q-mr-sm">
-            <q-tooltip>
-              {{ $t("book.retrieveBooksDialog.tooltip") }}
-            </q-tooltip>
-          </q-icon>
           <q-item-label>
+            <q-icon name="mdi-information-outline" class="q-mr-sm" size="18px">
+              <q-tooltip>
+                {{ $t("book.retrieveBooksDialog.tooltip") }}
+              </q-tooltip>
+            </q-icon>
             {{ $t("book.retrieveBooksDialog.retrieveAndPrint") }}
           </q-item-label>
         </q-btn>
-      </q-card-actions>
-    </q-card>
+      </template>
+    </k-dialog-card>
   </q-dialog>
 </template>
 
 <script setup lang="ts">
 import { useDialogPluginComponent } from "quasar";
+import KDialogCard from "../k-dialog-card.vue";
 
 const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
   useDialogPluginComponent();
