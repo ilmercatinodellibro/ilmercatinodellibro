f949e9c6a12178fed14fd6f2c054be90d7bf203a
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-02-23 17:24:53 +0300

    fix(server): resolve excessive stack depth comparing types error

--- packages/server/prisma/factories/event.ts
+++ packages/server/prisma/factories/event.ts
@@ -1,11 +1,11 @@
 import { faker } from "@faker-js/faker";
+import { Prisma } from "@prisma/client";
 import { random, startCase, times } from "lodash";
-import { EventCreateInput } from "src/@generated/event";
 import { createUser } from "./user";
 
 export async function createEvent(
-  overrides: Partial<EventCreateInput> = {},
-): Promise<EventCreateInput> {
+  overrides: Partial<Prisma.EventCreateInput> = {},
+): Promise<Prisma.EventCreateInput> {
   return {
     name: startCase(times(random(1, 4), () => faker.word.noun()).join(" ")),
     description: faker.lorem.sentence(),
