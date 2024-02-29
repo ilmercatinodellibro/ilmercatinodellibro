1e582783d4200a85c35df25aab6c6889ee5af81c
andrea-cerlini <84905818+andrea-cerlini@users.noreply.github.com>
2024-02-29 16:04:17 +0100

    refactor(table header): change event emission into form validation
    Co-authored-by: Yusuf Kandemir <y.kandemir@dreamonkey.com>

--- packages/client/src/components/manage-users/card-table-header.vue
+++ packages/client/src/components/manage-users/card-table-header.vue
@@ -1,21 +1,27 @@
 <template>
   <span class="gap-16 items-center q-pa-md row">
-    <q-input
-      v-model="bookISBN"
-      :placeholder="$t('manageUsers.searchHint')"
-      class="width-420"
-      debounce="200"
-      outlined
-      @keyup.enter="$emit('addBook', bookISBN)"
-    />
-    <q-btn
-      :label="$t('book.addBookDialog')"
-      :icon="mdiPlus"
-      color="accent"
-      no-wrap
-      @click="$emit('addBook', bookISBN)"
-    />
+    <q-form @submit="$emit('addBook', bookISBN)">
+      <q-input
+        v-model="bookISBN"
+        :placeholder="$t('manageUsers.searchHint')"
+        class="width-420"
+        :debounce="200"
+        :rules="[requiredRule]"
+        lazy-rules="ondemand"
+        outlined
+      />
+
+      <q-btn
+        type="submit"
+        :label="$t('book.addBookDialog')"
+        :icon="mdiPlus"
+        color="accent"
+        no-wrap
+      />
+    </q-form>
+
     <q-space />
+
     <slot name="side-actions" />
   </span>
 </template>
@@ -23,6 +29,7 @@
 <script setup lang="ts">
 import { mdiPlus } from "@quasar/extras/mdi-v7";
 import { ref } from "vue";
+import { requiredRule } from "src/helpers/rules";
 
 defineEmits<{
   addBook: [bookISBN: string];
