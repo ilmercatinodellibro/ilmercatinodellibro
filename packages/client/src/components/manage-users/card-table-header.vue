8a5a65ebd964c75b7b2179da95586889316fdf53
andrea.cerlini <andreacerlini2003@gmail.com>
2024-02-29 16:28:10 +0100

    feat(card table header): add ISBN validation rule

--- packages/client/src/components/manage-users/card-table-header.vue
+++ packages/client/src/components/manage-users/card-table-header.vue
@@ -1,35 +1,36 @@
 <template>
-  <span class="gap-16 items-center q-pa-md row">
-    <q-form @submit="$emit('addBook', bookISBN)">
-      <q-input
-        v-model="bookISBN"
-        :placeholder="$t('manageUsers.searchHint')"
-        class="width-420"
-        :debounce="200"
-        :rules="[requiredRule]"
-        lazy-rules="ondemand"
-        outlined
-      />
+  <q-form
+    class="gap-16 items-center q-pa-md row"
+    @submit="$emit('addBook', bookISBN)"
+  >
+    <q-input
+      v-model="bookISBN"
+      :placeholder="$t('manageUsers.searchHint')"
+      class="width-420"
+      :debounce="200"
+      :rules="[requiredRule, validISBN]"
+      lazy-rules="ondemand"
+      outlined
+    />
 
-      <q-btn
-        type="submit"
-        :label="$t('book.addBookDialog')"
-        :icon="mdiPlus"
-        color="accent"
-        no-wrap
-      />
-    </q-form>
+    <q-btn
+      type="submit"
+      :label="$t('book.addBookDialog')"
+      :icon="mdiPlus"
+      color="accent"
+      no-wrap
+    />
 
     <q-space />
 
     <slot name="side-actions" />
-  </span>
+  </q-form>
 </template>
 
 <script setup lang="ts">
 import { mdiPlus } from "@quasar/extras/mdi-v7";
 import { ref } from "vue";
-import { requiredRule } from "src/helpers/rules";
+import { requiredRule, validISBN } from "src/helpers/rules";
 
 defineEmits<{
   addBook: [bookISBN: string];
