b2ddef1c47f07bd9c9541a6bf806e976621d60af
andrea.cerlini <andreacerlini2003@gmail.com>
2024-03-12 16:44:36 +0100

    feat(client): add class filter

--- packages/client/src/models/book.ts
+++ packages/client/src/models/book.ts
@@ -2,3 +2,11 @@ export interface SchoolFilters {
   schoolCodes: string[];
   courses: string[];
 }
+
+export const COURSE_YEARS = [1, 2, 3, 4, 5] as const;
+
+export interface CourseDetails {
+  year: (typeof COURSE_YEARS)[number];
+  school: string;
+  course: string;
+}
