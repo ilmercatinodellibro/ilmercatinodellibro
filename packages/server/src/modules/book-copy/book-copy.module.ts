cd1b0e78a746a5f0c660c7b7ae4dafd019834aa3
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-03-15 12:55:24 +0300

    refactor(server): separate problem module

--- packages/server/src/modules/book-copy/book-copy.module.ts
+++ packages/server/src/modules/book-copy/book-copy.module.ts
@@ -2,11 +2,10 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
 import { BookCopyResolver } from "./book-copy.resolver";
 import { BookCopyService } from "./book-copy.service";
-import { ProblemResolver } from "./problem.resolver";
 
 @Module({
   imports: [PrismaModule],
-  providers: [BookCopyResolver, BookCopyService, ProblemResolver],
+  providers: [BookCopyResolver, BookCopyService],
   exports: [BookCopyService],
 })
 export class BookCopyModule {}
