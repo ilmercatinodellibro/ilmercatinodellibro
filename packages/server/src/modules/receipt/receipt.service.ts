33e3733f672a6d10dc81ca1f657f1c3fd1499f08
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-03-25 18:54:53 +0300

    feat(server): handle purchase receipt too

--- packages/server/src/modules/receipt/receipt.service.ts
+++ packages/server/src/modules/receipt/receipt.service.ts
@@ -4,9 +4,11 @@ import { GenerateProps } from "@pdfme/common";
 import { generate } from "@pdfme/generator";
 import { line, readOnlyText, table, text } from "@pdfme/schemas";
 import { Book, BookCopy, Sale } from "@prisma/client";
+import { sumBy } from "lodash";
 import { ReceiptType } from "src/@generated";
 import { PrismaService } from "src/modules/prisma/prisma.service";
-import registeredTemplate from "./templates/registered.json";
+import purchaseTemplate from "./templates/purchase.json";
+import registrationTemplate from "./templates/registration.json";
 
 type BookCopyWithBook = BookCopy & { book: Book };
 export type CreateReceiptInput = {
@@ -65,21 +67,27 @@ export class ReceiptService {
       },
     });
 
-    // TODO: Handle other types of receipts
-    const receiptPdf = await this.generateRegistrationReceipt({
+    const books = (
+      type === ReceiptType.PURCHASE
+        ? data.map(({ bookCopy }) => bookCopy)
+        : data
+    ).map(({ code, book }) => ({
+      isbnCode: book.isbnCode,
+      title: book.title,
+      subject: book.subject,
+      originalPrice: book.originalPrice,
+      code,
+    }));
+
+    const generateReceipt =
+      type === ReceiptType.PURCHASE
+        ? this.generatePurchaseReceipt.bind(this)
+        : this.generateRegistrationReceipt.bind(this);
+    const receiptPdf = await generateReceipt({
       creationDate: receipt.createdAt,
       locationName: receipt.retailLocation.name,
       userEmail: receipt.user.email,
-      books: (type === ReceiptType.PURCHASE
-        ? data.map(({ bookCopy }) => bookCopy)
-        : data
-      ).map(({ code, book }) => ({
-        isbnCode: book.isbnCode,
-        title: book.title,
-        subject: book.subject,
-        originalPrice: book.originalPrice,
-        code,
-      })),
+      books,
     });
     // TODO: Make the directory path dynamic
     await writeFile(`./tmp-files/receipts/${receipt.id}.pdf`, receiptPdf);
@@ -93,13 +101,7 @@ export class ReceiptService {
     locationName,
     books,
   }: GenerateReceiptInput) {
-    const formattedDate = creationDate.toLocaleDateString("it-IT", {
-      hour: "numeric",
-      minute: "numeric",
-      day: "numeric",
-      month: "numeric",
-      year: "numeric",
-    });
+    const formattedDate = this.#formatCreationDate(creationDate);
     const bookRows = books.map((book) => [
       book.isbnCode,
       book.title,
@@ -109,9 +111,9 @@ export class ReceiptService {
     ]);
     const collectionPeriod = this.#getFormattedCollectionPeriod();
 
-    const schema = registeredTemplate.schemas[0];
+    const schema = registrationTemplate.schemas[0];
     return await generate({
-      template: registeredTemplate as unknown as GenerateProps["template"],
+      template: registrationTemplate as unknown as GenerateProps["template"],
       inputs: [
         {
           headerSubtitle: schema.headerSubtitle.content.replace(
@@ -144,6 +146,56 @@ export class ReceiptService {
     });
   }
 
+  async generatePurchaseReceipt({
+    creationDate,
+    userEmail,
+    locationName,
+    books,
+  }: GenerateReceiptInput) {
+    const formattedDate = this.#formatCreationDate(creationDate);
+    const bookRows = books.map((book) => [
+      book.isbnCode,
+      book.title,
+      book.subject,
+      book.originalPrice.toFixed(2),
+    ]);
+
+    const schema = purchaseTemplate.schemas[0];
+    return await generate({
+      template: purchaseTemplate as unknown as GenerateProps["template"],
+      inputs: [
+        {
+          headerSubtitle: schema.headerSubtitle.content.replace(
+            "{location}",
+            locationName,
+          ),
+          headerDate: schema.headerDate.content.replace(
+            "{date}",
+            formattedDate,
+          ),
+          title: schema.title.content.replace("{email}", userEmail),
+          table: JSON.stringify(bookRows),
+          notice: schema.notice.content.replace(
+            "{totalPrice}",
+            sumBy(books, "originalPrice").toFixed(2),
+          ),
+        },
+      ],
+      options: {
+        creationDate,
+        language: "it",
+        title: schema.headerTitle.content,
+        // TODO: Use appropriate fonts, weights, etc.
+      },
+      plugins: {
+        text,
+        readOnlyText,
+        line,
+        table,
+      },
+    });
+  }
+
   #getFormattedCollectionPeriod() {
     const { from, to } = this.#collectionPeriod;
     return {
@@ -156,5 +208,15 @@ export class ReceiptService {
         month: "long",
       }),
     };
+  }
+
+  #formatCreationDate(date: Date) {
+    return date.toLocaleDateString("it-IT", {
+      hour: "numeric",
+      minute: "numeric",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
   }
 }
