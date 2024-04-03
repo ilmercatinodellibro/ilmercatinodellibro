1da9feab9ca0c2b1613af3ba537baa9e2c0d96cf
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-04-03 12:48:23 +0300

    refactor(client): extract theme color sections to a component

---
+++ packages/client/src/components/theme-color-section.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="items-center justify-between row">
+    <div class="items-start row">
+      <div :class="`bg-${name}`" class="circle q-ml-sm q-mt-sm"></div>
+      <div class="column q-ml-xl">
+        <span class="line-height-75 text-size-16">
+          {{ t(`general.${name}`) }}
+        </span>
+        <span class="text-black-54 text-subtitle">
+          {{ theme.colors[name] }}
+        </span>
+        <span class="text-black-54 text-subtitle">
+          {{ RGBTheme[name] }}
+        </span>
+      </div>
+    </div>
+
+    <q-btn color="black-12" outline @click="openColorPickerDialog(name)">
+      <span class="text-black-87">
+        {{ t("common.edit") }}
+      </span>
+    </q-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Dialog } from "quasar";
+import { useI18n } from "vue-i18n";
+import ColorPickerDialog from "src/components/color-picker-dialog.vue";
+import { ThemeColor, useTheme } from "src/composables/use-theme";
+
+defineProps<{
+  name: ThemeColor;
+}>();
+
+const { t } = useI18n();
+
+const { theme, RGBTheme } = useTheme();
+
+function openColorPickerDialog(colorType: ThemeColor) {
+  Dialog.create({
+    component: ColorPickerDialog,
+    componentProps: {
+      color: theme.value.colors[colorType],
+    },
+  }).onOk((newColor: string) => {
+    theme.value.colors[colorType] = newColor;
+  });
+}
+</script>
+
+<style lang="scss" scoped>
+.circle {
+  border-radius: 50px;
+  height: 24px;
+  width: 24px;
+}
+</style>
--- packages/client/src/pages/theme.vue
+++ packages/client/src/pages/theme.vue
@@ -14,8 +14,10 @@
             @click="saveChanges()"
           />
         </q-card-section>
+
         <q-separator />
       </template>
+
       <q-card-section
         :class="
           !isSmallScreen ? 'row q-pl-xl q-ml-xl' : 'column justify-center'
@@ -28,6 +30,7 @@
             {{ t("general.logoSizeMessage") }}
           </p>
         </div>
+
         <div
           class="column gap-16 justify-center"
           :class="isSmallScreen ? 'full-width' : 'q-mb-xl'"
@@ -35,6 +38,7 @@
           <q-btn color="black-12" outline @click="imagePickerRef.pickFiles()">
             <span class="text-black-87">{{ t("general.uploadLogo") }}</span>
           </q-btn>
+
           <q-btn
             color="black-12"
             outline
@@ -43,6 +47,7 @@
             <span class="text-black-87"> {{ t("general.resetLogo") }} </span>
           </q-btn>
         </div>
+
         <q-file
           ref="imagePickerRef"
           v-model="imagePickerModel"
@@ -51,87 +56,18 @@
           @rejected="onRejected"
         />
       </q-card-section>
+
       <q-card-section class="column gap-16">
-        <div class="items-center justify-between row">
-          <div class="items-start row">
-            <div class="bg-primary circle q-ml-sm q-mt-sm"></div>
-            <div class="column q-ml-xl">
-              <span class="line-height-75 text-size-16">{{
-                t("general.primary")
-              }}</span>
-              <span class="text-black-54 text-subtitle">
-                {{ theme.colors.primary }}
-              </span>
-              <span class="text-black-54 text-subtitle">
-                {{ RGBTheme.primary }}
-              </span>
-            </div>
-          </div>
-          <q-btn
-            color="black-12"
-            outline
-            @click="openColorPickerDialog('primary')"
-          >
-            <span class="text-black-87">
-              {{ t("common.edit") }}
-            </span>
-          </q-btn>
-        </div>
+        <theme-color-section name="primary" />
         <q-separator />
-        <div class="items-center justify-between row">
-          <div class="items-start row">
-            <div class="bg-secondary circle q-ml-sm q-mt-sm"></div>
-            <div class="column q-ml-xl">
-              <span class="line-height-75 text-size-16">{{
-                t("general.secondary")
-              }}</span>
-              <span class="text-black-54 text-subtitle">
-                {{ theme.colors.secondary }}
-              </span>
-              <span class="text-black-54 text-subtitle">
-                {{ RGBTheme.secondary }}
-              </span>
-            </div>
-          </div>
-          <q-btn
-            color="black-12"
-            outline
-            @click="openColorPickerDialog('secondary')"
-          >
-            <span class="text-black-87">
-              {{ t("common.edit") }}
-            </span>
-          </q-btn>
-        </div>
+        <theme-color-section name="secondary" />
         <q-separator />
-        <div class="items-center justify-between row">
-          <div class="items-start row">
-            <div class="bg-accent circle q-ml-sm q-mt-sm"></div>
-            <div class="column q-ml-xl">
-              <span class="line-height-75 text-size-16">{{
-                t("general.accent")
-              }}</span>
-              <span class="text-black-54 text-subtitle">
-                {{ theme.colors.accent }}
-              </span>
-              <span class="text-black-54 text-subtitle">
-                {{ RGBTheme.accent }}
-              </span>
-            </div>
-          </div>
-          <q-btn
-            color="black-12"
-            outline
-            @click="openColorPickerDialog('accent')"
-          >
-            <span class="text-black-87">
-              {{ t("common.edit") }}
-            </span>
-          </q-btn>
-        </div>
+        <theme-color-section name="accent" />
       </q-card-section>
+
       <q-page-sticky v-if="isSmallScreen" expand position="bottom">
         <q-separator class="full-width" />
+
         <div class="full-width q-pa-md">
           <q-btn
             class="full-width"
@@ -151,21 +87,15 @@ import { Dialog, QFile, Screen } from "quasar";
 import { Ref, computed, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { onBeforeRouteLeave } from "vue-router";
-import ColorPickerDialog from "components/color-picker-dialog.vue";
 import ConfirmDialog from "src/components/confirm-dialog.vue";
-import { ThemeColor, useTheme } from "src/composables/use-theme";
+import ThemeColorSection from "src/components/theme-color-section.vue";
+import { useTheme } from "src/composables/use-theme";
 import { notifyError } from "src/helpers/error-messages";
 import { AvailableRouteNames } from "src/models/routes";
 
 const { t } = useI18n();
-const {
-  theme,
-  defaultTheme,
-  RGBTheme,
-  hasPendingChanges,
-  saveChanges,
-  setDefaults,
-} = useTheme();
+const { theme, defaultTheme, hasPendingChanges, saveChanges, setDefaults } =
+  useTheme();
 
 const imagePickerModel = ref<File>();
 const imagePickerRef = ref() as Ref<QFile>;
@@ -191,17 +121,6 @@ onBeforeRouteLeave((_to, from, next) => {
     });
 });
 
-function openColorPickerDialog(colorType: ThemeColor) {
-  Dialog.create({
-    component: ColorPickerDialog,
-    componentProps: {
-      color: theme.value.colors[colorType],
-    },
-  }).onOk((newColor: string) => {
-    theme.value.colors[colorType] = newColor;
-  });
-}
-
 function onRejected() {
   notifyError("Uploaded file did not pass validation check");
 }
@@ -225,12 +144,6 @@ watch([imagePickerModel], ([newImage]) => {
 <style lang="scss" scoped>
 .theme-card {
   min-width: 50vw;
-}
-
-.circle {
-  border-radius: 50px;
-  height: 24px;
-  width: 24px;
 }
 
 .logo-container {
