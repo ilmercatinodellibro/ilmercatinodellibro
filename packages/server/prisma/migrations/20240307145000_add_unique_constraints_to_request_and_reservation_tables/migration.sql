7284b1deafc849651bb3305169a9611903634bcf
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-04-30 12:59:41 +0300

    fix(server): fix book request unique constraint

--- packages/server/prisma/migrations/20240307145000_add_unique_constraints_to_request_and_reservation_tables/migration.sql
+++ packages/server/prisma/migrations/20240307145000_add_unique_constraints_to_request_and_reservation_tables/migration.sql
@@ -1,12 +1,13 @@
 /*
   Warnings:
-
   - A unique constraint covering the columns `[user_id,book_id,deleted_at]` on the table `BookRequest` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[user_id,book_id,deleted_at]` on the table `Reservation` will be added. If there are existing duplicate values, this will fail.
-
 */
--- CreateIndex
-CREATE UNIQUE INDEX "BookRequest_user_id_book_id_deleted_at_key" ON "BookRequest"("user_id", "book_id", "deleted_at");
+
+-- `NULLS NOT DISTINCT` was added manually, otherwise NULL values will be considered all unique, which defeats the purpose of the constraint
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Reservation_user_id_book_id_deleted_at_key" ON "Reservation"("user_id", "book_id", "deleted_at");
+CREATE UNIQUE INDEX "BookRequest_user_id_book_id_deleted_at_key" ON "BookRequest"("user_id", "book_id", "deleted_at") NULLS NOT DISTINCT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Reservation_user_id_book_id_deleted_at_key" ON "Reservation"("user_id", "book_id", "deleted_at") NULLS NOT DISTINCT;
