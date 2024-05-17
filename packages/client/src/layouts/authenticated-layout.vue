beb64a84d051a5d69470dcd8fbbb44580a943974
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2024-05-17 21:24:55 +0300

    refactor(client): simplify location settings cache management

--- packages/client/src/layouts/authenticated-layout.vue
+++ packages/client/src/layouts/authenticated-layout.vue
@@ -415,7 +415,6 @@ import { useAuthService, useLogoutMutation } from "src/services/auth";
 import { useRetailLocationService } from "src/services/retail-location";
 import {
   RetailLocationSettingsFragment,
-  RetailLocationSettingsFragmentDoc,
   useUpdateRetailLocationSettingsMutation,
 } from "src/services/retail-location.graphql";
 import {
@@ -483,21 +482,12 @@ function openSettings() {
   }).onOk(async (payload: SettingsUpdate) => {
     if (payload.type === "save") {
       try {
-        const { cache } = await updateRetailLocationSettings({
+        await updateRetailLocationSettings({
           input: {
             ...payload.settings,
             retailLocationId: selectedLocation.value.id,
           },
         });
-
-        cache.updateFragment(
-          {
-            fragment: RetailLocationSettingsFragmentDoc,
-            fragmentName: "RetailLocationSettings",
-            id: cache.identify(selectedLocation.value),
-          },
-          () => ({ ...payload.settings }),
-        );
       } catch {
         notifyError(t("common.genericErrorMessage"));
       }
