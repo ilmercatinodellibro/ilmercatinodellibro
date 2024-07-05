67a7eb0eb3c878c9a962cd55ba7efc685ea7f9db
Paolo Caleffi <p.caleffi@dreamonkey.com>
2024-07-05 12:54:47 +0200

    fix(auth): send locale while registering

--- packages/client/src/pages/register.vue
+++ packages/client/src/pages/register.vue
@@ -157,7 +157,7 @@ const props = defineProps<{
 
 const SOCIAL_LOGIN_ENABLED = process.env.SOCIAL_LOGIN_ENABLED === "true";
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
 const { theme } = useTheme();
 
@@ -285,6 +285,7 @@ async function onSubmit() {
         ...omit(user.value, ["confirmEmail"]),
         dateOfBirth: Date.parse(user.value.dateOfBirth),
         retailLocationId: selectedLocation.value.id,
+        locale: locale.value,
       },
     };
 
