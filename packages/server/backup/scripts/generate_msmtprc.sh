e484529ff9b8648dcb73997033301447868dcf92
MarinCervinschi <marincervinschi3@gmail.com>
2025-04-30 12:55:32 +0200

    ci(msmtprc): generate_msmtprc script based on MAIL_SECURE flag

---
+++ packages/server/backup/scripts/generate_msmtprc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$MAIL_SECURE" = "true" ]; then
+    export TLS_ENABLED="on"
+    export TLS_TRUST_FILE="/etc/ssl/certs/ca-certificates.crt"
+else
+    export TLS_ENABLED="off"
+    export TLS_TRUST_FILE=""
+fi
+
+# Generate the msmtprc file
+envsubst < /etc/msmtprc_template > /etc/msmtprc
+chmod 600 /etc/msmtprc
