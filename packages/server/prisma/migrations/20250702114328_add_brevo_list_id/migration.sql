492bb08377f6d1c74e1b0cafcc28b320295ff5a7
andrea.cerlini <84905818+andrea-cerlini@users.noreply.github.com>
2025-07-02 18:10:04 +0200

    fix(server): update each retail location separately + add brevo contacts list id for each location

---
+++ packages/server/prisma/migrations/20250702114328_add_brevo_list_id/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "RetailLocation" ADD COLUMN     "brevo_contacts_list_id" TEXT NOT NULL DEFAULT '';
--- packages/server/src/modules/user/user.module.ts
+++ packages/server/src/modules/user/user.module.ts
@@ -3,9 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "src/modules/auth/auth.module";
 import { AddAdminUserCommand } from "src/modules/user/commands/add-admin-user.command";
 import { SeedUsersWithBooksCommand } from "src/modules/user/commands/seed-users-with-books.command";
-import { UploadUsersCommand } from "src/modules/user/commands/sync-users.command";
 import { NewsletterContactsService } from "src/modules/user/newsletter-contacts.service";
-import { SyncUsersCommand } from "src/modules/user/sync-users.command";
 import { UserAccountResolver } from "src/modules/user/user-account.resolver";
 import { UserController } from "src/modules/user/user.controller";
 import { PrismaModule } from "../prisma/prisma.module";
@@ -20,8 +18,6 @@ import { UserService } from "./user.service";
     UserService,
     AddAdminUserCommand,
     SeedUsersWithBooksCommand,
-    SyncUsersCommand,
-    UploadUsersCommand,
     NewsletterContactsService,
   ],
   exports: [UserService],
