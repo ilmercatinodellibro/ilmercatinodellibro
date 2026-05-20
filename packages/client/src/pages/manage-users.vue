9b847ef46e575c25852abea0c002f2328818ba06
Paolo Caleffi <p.caleffi@dreamonkey.com>
2026-05-20 17:18:51 +0200

    refactor(manage-users): polish user row highlight

--- packages/client/src/pages/manage-users.vue
+++ packages/client/src/pages/manage-users.vue
@@ -82,9 +82,9 @@
                   : `user-row-${props.row.id}`
               "
               :tabindex="props.rowIndex"
-              @focusin="toggleSelectedClass(props.row.id, true)"
-              @blur="toggleSelectedClass(props.row.id)"
-              @keydown.escape="toggleSelectedClass(props.row.id)"
+              @focusin="updateSelectedStatus(props.row.id, 'selected')"
+              @blur="updateSelectedStatus(props.row.id, 'deselected')"
+              @keydown.escape="updateSelectedStatus(props.row.id, 'deselected')"
             >
               <q-td key="edit" :props>
                 <q-btn
@@ -502,16 +502,26 @@ onMounted(() => {
   tableRef.value.requestServerInteraction();
 });
 
-// Since for the table we are using the body slot, referencing a variable
-// from the template would cause a re-render of all rows, which heavily
-// impacted performance
-function toggleSelectedClass(rowId: string, forceState?: boolean) {
+// Since we are using the table body slot, using a reactive ref to the table (e.g. tableRef)
+// would cause a massive re-render of all rows for every access, causing a huge performance issue
+// That's why we directly manipulate the DOM to toggle the selected class on focus/blur/escape
+function updateSelectedStatus(
+  rowId: string,
+  newStatus: "selected" | "deselected",
+) {
   const tableRow = document.getElementsByClassName(`user-row-${rowId}`)[0];
   if (!tableRow) {
     return;
   }
 
-  tableRow.classList.toggle("bg-primary-selected", forceState);
+  switch (newStatus) {
+    case "selected":
+      tableRow.classList.add("bg-primary-selected");
+      break;
+    case "deselected":
+      tableRow.classList.remove("bg-primary-selected");
+      break;
+  }
 }
 
 const onRequest: QTableProps["onRequest"] = async (requested) => {
